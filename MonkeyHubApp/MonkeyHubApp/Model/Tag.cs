8d933581b756d6e9dc76cf9373ae316bceed28ea
rubgithub <rubensatual@hotmail.com>
2017-05-14 16:55:15 -0400

    Coleções com classes customizadas

---
+++ MonkeyHubApp/MonkeyHubApp/Model/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace MonkeyHubApp.Model
+{
+    public class Tag
+    {
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string Id { get; set; }
+    }
+}
--- MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using MonkeyHubApp.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MonkeyHubApp.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        private const string BaseUrl = "https://monkey-hub-api.azurewebsites.net/api/";
+
         private string _searchTerm;
         public string SearchTerm
         {
@@ -16,16 +25,36 @@ namespace MonkeyHubApp.ViewModels
             }
         }
 
-        public ObservableCollection<string> Resultados { get; }
+        public ObservableCollection<Tag> Resultados { get; }
 
         public Command SearchCommand { get; }
 
         public MainViewModel()
         {
             SearchCommand = new Command(ExecuteSearchCommand, CanExecuteSearchCommand);
-            Resultados = new ObservableCollection<string>() {"abc", "cde" };
+            Resultados = new ObservableCollection<Tag>();
         }
-       
+
+        public async Task<List<Tag>> GetTagsAsync()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await httpClient.GetAsync($"{BaseUrl}Tags").ConfigureAwait(false);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using (var responseStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                {
+                    return JsonConvert.DeserializeObject<List<Tag>>(
+                        await new StreamReader(responseStream)
+                            .ReadToEndAsync().ConfigureAwait(false));
+                }
+            }
+
+            return null;
+        }
+
         async void ExecuteSearchCommand()
         {
             //App.Current.MainPage - não recomendado pois código fica acoplado não permitindo testes
@@ -33,11 +62,16 @@ namespace MonkeyHubApp.ViewModels
             if (resposta)
             {
                 await App.Current.MainPage.DisplayAlert("MonkeyHubApp", "Obrigado por pesquisar", "OK");
-                Resultados.Add("Sim");
-            }
-            else
-            {
-                Resultados.Add("Não");
+
+                Resultados.Clear();
+                var tagsRetornadas = await GetTagsAsync();
+                if (tagsRetornadas != null)
+                {
+                    foreach (var tag in tagsRetornadas)
+                    {
+                        Resultados.Add(tag);
+                    }
+                }
             }
 
         }
