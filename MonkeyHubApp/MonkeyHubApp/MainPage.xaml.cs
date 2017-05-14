2507665f40c6ff34d728a9a0d7ef62511622f60f
rubgithub <rubensatual@gmail.com>
2017-05-14 14:17:59 -0400

    Adicionar arquivos de projeto.

---
+++ MonkeyHubApp/MonkeyHubApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MonkeyHubApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MonkeyHubApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace MonkeyHubApp.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {        
+        
+    }
+}
+
+/*
+private string _descricao;
+public string Descricao
+{
+    get { return _descricao; }
+    set
+    {
+        SetProperty(ref _descricao, value);
+        //_descricao = value;
+        //OnPropertyChanged();
+    }
+}
+
+public MainViewModel()
+{
+    Descricao = "Olá mundo I'm here...";
+}
+*/
