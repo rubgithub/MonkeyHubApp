01599131af0a1f185e6c868ec36cd5ae5023e9ba
rubgithub <rubensatual@gmail.com>
2017-05-14 15:30:15 -0400

    Command e Alerts

--- MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -1,8 +1,42 @@
-﻿namespace MonkeyHubApp.ViewModels
+﻿using System;
+using Xamarin.Forms;
+
+namespace MonkeyHubApp.ViewModels
 {
     public class MainViewModel : BaseViewModel
-    {        
-        
+    {
+        private string _searchTerm;
+        public string SearchTerm
+        {
+            get { return _searchTerm; }
+            set {
+                if(SetProperty(ref _searchTerm, value))
+                    SearchCommand.ChangeCanExecute();
+            }
+        }
+
+        public Command SearchCommand { get; }
+
+        public MainViewModel()
+        {
+            SearchCommand = new Command(ExecuteSearchCommand, CanExecuteSearchCommand);
+        }
+       
+        async void ExecuteSearchCommand()
+        {
+            //App.Current.MainPage - não recomendado pois código fica acoplado não permitindo testes
+            var resposta = await App.Current.MainPage.DisplayAlert("MonkeyHubApp", $"Você procurou por: '{SearchTerm}'", "Sim", "Não");
+            if (resposta)
+            {
+                await App.Current.MainPage.DisplayAlert("MonkeyHubApp", "Obrigado por pesquisar", "OK");
+            }
+        }
+
+        bool CanExecuteSearchCommand()
+        {
+            return !string.IsNullOrWhiteSpace(SearchTerm);
+        }
+
     }
 }
 
